0505875ad381a5c45dc5057b2a79ae2b9da6d1dc
MabMab^^ <miguelberrioh@hotmail.com>
2025-06-28 18:35:27 -0500

    Update clase 12

--- clase-12/script.js
+++ clase-12/script.js
@@ -1,6 +1,16 @@
 console.log("Mensaje de prueba");
 //llamadoDeAlerta("Miguel", 28);
-agregarEstiloAImagenes();
+//agregarEstiloAImagenes();
+document.getElementById("btnCalcular").setAttribute("onclick", "calcularSuma()");
+//calcularSuma();
+
+function calcularSuma(){
+    let numero1 = parseFloat(document.getElementById("numero1").value); // 123
+    let numero2 = parseFloat(document.getElementById("numero2").value); // 2
+    document.getElementById("suma").value = numero1+numero2; // 125
+    console.log(numero1)
+    console.log(numero2)
+}
 
 function llamadoDeAlerta(nombre, edad){
     alert(`El nombre es ${nombre} y la edad ${edad}`)

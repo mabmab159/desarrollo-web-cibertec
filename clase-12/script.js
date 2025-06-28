69bf8b9f2f57c2be32c156a1314d62bbd7a388c5
MabMab^^ <miguelberrioh@hotmail.com>
2025-06-28 18:13:26 -0500

    Add clase 12

---
+++ clase-12/script.js
@@ -0,0 +1,16 @@
+console.log("Mensaje de prueba");
+//llamadoDeAlerta("Miguel", 28);
+agregarEstiloAImagenes();
+
+function llamadoDeAlerta(nombre, edad){
+    alert(`El nombre es ${nombre} y la edad ${edad}`)
+}
+
+function agregarEstiloAImagenes(){
+   let cajas = document.getElementsByClassName("caja1");
+   let cajas2 = document.getElementById("caja2");
+   document.getElementsByTagName
+   console.log(cajas)
+   console.log(cajas[0])
+   console.log(cajas2)
+}
